db5b52d97a9e985de6f07d4f11c9beeb29ddb873
victor benavente <templario0141@gmail.com>
2025-10-07 11:44:32 -0500

    feat: add logs to jobs

--- src/modules/market-reminder/infrastructure/input/market-reminder.controller.ts
+++ src/modules/market-reminder/infrastructure/input/market-reminder.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Head, HttpCode, HttpStatus } from '@nestjs/common';
+import { Controller, Head, HttpCode, HttpStatus, Logger } from '@nestjs/common';
 import { SyncPricesForAllQuotesUseCase } from '../../application/create-quote-prices.use-case';
 import { NotifyQuotationStatsToUsersUseCase } from '../../application/notify-quotation-stats-to-users.use-case';
 
 @Controller('market-reminder')
 export class MarketReminderController {
+  private readonly logger = new Logger(MarketReminderController.name);
   constructor(
     private readonly syncPricesForAllQuotesUseCase: SyncPricesForAllQuotesUseCase,
     private readonly notifyQuotationStatsToUsersUseCase: NotifyQuotationStatsToUsersUseCase,
@@ -12,12 +13,14 @@ export class MarketReminderController {
   @HttpCode(HttpStatus.ACCEPTED)
   @Head('sync-quote-prices')
   syncQuotePrices() {
+    this.logger.log('Starting quote prices synchronization...');
     this.syncPricesForAllQuotesUseCase.execute();
   }
 
   @HttpCode(HttpStatus.ACCEPTED)
   @Head('notify-user-reports')
   notifyUserReports() {
+    this.logger.log('Starting user reports notification...');
     this.notifyQuotationStatsToUsersUseCase.execute();
   }
 }
